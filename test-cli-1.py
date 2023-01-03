6350cf240f946c86632dd3678fe11b28458f7266
Bogdan Khrupa <lizard.freddi@gmail.com>
2023-01-03 21:57:52 +0200

    if

--- test-cli-1.py
+++ test-cli-1.py
@@ -1,6 +1,6 @@
 
 
-def pr(name='Default', age='Unknown'):
+def pr_name_and_age(name='Default', age='Unknown'):
     print('Name', name, 'age:', age)
 
 
@@ -9,9 +9,9 @@ def pr_people(*people):
         print(person)
 
 
-pr(age='23', name='Foo')
-pr('AAA', 18)
-pr()
+pr_name_and_age(age='23', name='Foo')
+pr_name_and_age('AAA', 18)
+pr_name_and_age()
 pr_people('Odin', 'Thor', 'Loki')
 
 # ---
@@ -27,3 +27,15 @@ sum1 = math_sum(3, 5);
 print(sum1)
 print(math_sum(sum1, 2))
 
+
+# ---
+print('- - - - -')
+
+check = 'thor'
+
+if check == 'odin':
+    print('is Odin')
+elif check == 'thor':
+    print('is Thor')
+else:
+    print('unknown')
