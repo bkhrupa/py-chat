6fe351972d4e6139a3868c644cdb42e57345e7c2
Bogdan Khrupa <lizard.freddi@gmail.com>
2023-01-03 21:51:07 +0200

    return

--- test-cli-1.py
+++ test-cli-1.py
@@ -12,5 +12,18 @@ def pr_people(*people):
 pr(age='23', name='Foo')
 pr('AAA', 18)
 pr()
-
 pr_people('Odin', 'Thor', 'Loki')
+
+# ---
+print('- - - - -')
+
+
+def math_sum(num1, num2):
+    return num1 + num2
+
+
+print(math_sum(1, 2))
+sum1 = math_sum(3, 5);
+print(sum1)
+print(math_sum(sum1, 2))
+
