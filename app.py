c30e4abae53a214c8ed209be7d79c8f1a36ba89f
Bogdan Khrupa <lizard.freddi@gmail.com>
2023-01-12 00:32:28 +0200

    templates and styles.

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 # save this as app.py
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
