a9db8fcc8789f8e164dec37d9a2842721bf82aaf
Bogdan Khrupa <lizard.freddi@gmail.com>
2023-01-19 09:31:30 +0200

    database.

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-# save this as app.py
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database/database.sqlite"
+db = SQLAlchemy(app)
+db.init_app(app)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return '<Message %r>' % self.id
 
 
 @app.route("/")
